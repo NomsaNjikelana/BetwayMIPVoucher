87dec123c5e8690faa802f2ee0ed9cb62849c906
nomsa.njikelana <nomsaakhonakhumalo@gmail.com>
2025-03-13 09:46:48 +0200

    Added Code to Listerner

--- src/test/java/ExtentReport/Listener.java
+++ src/test/java/ExtentReport/Listener.java
@@ -1,4 +1,36 @@
 package ExtentReport;
 
-public class Listener {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listener implements ITestListener {
+
+    private static ExtentReports extent;
+    private static ExtentTest extentTest;
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        extentTest = extent.createTest(result.getMethod().getMethodName());
+    }
+    @Override
+    public void onTestFailure(ITestResult result) {
+        extentTest.log(Status.FAIL, "Invalid Voucher Test Case " +result.getMethod().getMethodName() + " Has Failed");
+    }
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        extentTest.log(Status.FAIL, "Invalid Voucher Test Case " +result.getMethod().getMethodName() + " Has Passed");
+    }
+    @Override
+    public void onFinish(ITestContext result){
+        extent.flush();
+    }
+
+    public void onStart(ITestContext result){
+        extent = ExtentReportManager.extentSetUp();
+    }
+
 }
